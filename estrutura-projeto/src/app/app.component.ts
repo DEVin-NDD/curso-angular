944c63a116254f1f8688cef2bf161c3615ccbc0b
Kelvis Borges <tp.kelvis.borges@d0007azurecloudtotvscom.onmicrosoft.com>
2022-04-20 15:44:58 -0300

    initial commit

---
+++ estrutura-projeto/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'esp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'estrutura-projeto';
+}
